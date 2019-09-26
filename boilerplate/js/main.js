b3b17e666634a8689ad874480772c0f024bc2989
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-09-26 09:43:38 +0530

    digital clock done

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,1 +1,9 @@
 // You js goes here
+
+
+setInterval(() => {
+    var time = document.querySelector('.head');
+    var d = new Date();
+    var n = d.toLocaleTimeString();
+    time.textContent = n;  
+}, 1000);
